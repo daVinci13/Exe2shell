049e4cdf9851c75e318030351bd1ba9f887c338b
m <146552991+mrky404@users.noreply.github.com>
2023-10-03 15:45:47 +0200

    Update exe2shell.py

--- exe2shell.py
+++ exe2shell.py
@@ -1,6 +1,17 @@
-fn = input("Enter the file location: ")
+def read_file(file_location):
+    with open(file_location, 'rb') as file:
+        content = bytearray(file.read())
+    return content
 
-with open(fn, 'rb') as f:
-    sc = bytearray(f.read())
+def format_content(content):
+    formatted_content = '\\x' + '\\x'.join(format(byte, '02x') for byte in content)
+    return formatted_content
 
-print('\\x' + '\\x'.join(format(x, '02x') for x in sc))
+def main():
+    file_location = input("Enter the file location: ")
+    content = read_file(file_location)
+    formatted_content = format_content(content)
+    print(formatted_content)
+
+if __name__ == "__main__":
+    main()
